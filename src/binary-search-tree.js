55d2ea193e0706b37145d581523de1c3a3d86331
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2023-12-24 22:11:36 +0300

    fix: fix bst

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,11 +1,42 @@
 const { NotImplementedError } = require("../extensions/index.js");
 
-const { Node } = require("../extensions/list-tree.js");
+// const { Node } = require("../extensions/list-tree.js");
 
 /**
  * Implement simple binary search tree according to task description
  * using Node from extensions
  */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  add(data) {
+    if (data < this.data) {
+      this.addLeft(data);
+    } else {
+      this.addRight(data);
+    }
+  }
+
+  addLeft(data) {
+    if (this.left) {
+      return this.left.add(data);
+    }
+    this.left = new Node(data);
+  }
+
+  addRight(data) {
+    if (this.right) {
+      return this.right.add(data);
+    }
+    this.right = new Node(data);
+  }
+}
+
 class BinarySearchTree {
   constructor() {
     this.rootValue = null;
